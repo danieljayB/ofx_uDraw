266012913935646a4c8c635825abb0726380289f
Danny <danbertner@gmail.com>
2014-09-30 13:58:07 -0400

    updated readme

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ ofx_uDraw
 
 An  (in progress) openFrameworks addon for the uDraw Playstation 3 game tablet. driver Compatibility only for Linux.
 
+Daniel Bertner 2013
--- src/evdev.hpp
+++ src/evdev.hpp
@@ -14,6 +14,7 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
 #ifndef HEADER_EVDEV_HPP
 #define HEADER_EVDEV_HPP
 
